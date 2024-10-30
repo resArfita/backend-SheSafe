4fd95271199e90fdf1c8fb94ce0096c28b840ddb
Rusydina Novitasari <rnvitas697@gmail.com>
2024-10-30 14:46:43 +0700

    resolved error in the routes/index.js

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/Admin");
 
 module.exports = {
   getUser: async (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ const communityRoute = require("./community-route");
 const authRoute = require("./auth-route");
 const catRoute = require("./category-route");
 const casesRoute = require("./cases-route");
-const authMiddleware = require("../middleware/auth");
 const adminRoute = require("./admin-route");
+const { validateToken } = require("../middleware/auth");
 
 // route.use("/eduShesafe", eduRoute);
 route.use("/auth", authRoute);
 route.use("/community", communityRoute);
-route.use("/cases", authMiddleware, casesRoute);
+route.use("/cases", validateToken, casesRoute);
 route.use("/category", catRoute);
-route.use("/admin", catRoute);
+route.use("/admin", adminRoute);
 
 module.exports = route;
