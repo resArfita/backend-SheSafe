7bcf10ec9ac31b1efd793e90f2b74fea1e4446c6
Rusydina Novitasari <rnvitas697@gmail.com>
2024-11-02 18:56:13 +0700

    Add filter by status to getCases; implement getJournalCasesById, getJournalById, and addCasesDraft; adjust cases-route

--- controllers/cases-controller.js
+++ controllers/cases-controller.js
@@ -1,15 +1,55 @@
 const Cases = require("../models/Cases");
+const Journal = require("../models/Journal");
 
 module.exports = {
   getCases: async (req, res) => {
     const { userId } = req.user;
+    const Status = req.query.status || "";
 
-    const data = await Cases.find({ createdBy: userId });
+    const data = await Cases.find({
+      createdBy: userId,
+      isApproved: Status,
+    }).sort({
+      created: "desc",
+    });
 
     res.json({
       message: "berhasil mendapatkan data semua user",
       data,
     });
+  },
+
+  //ajukan kasus dengan get data jurnal dulu , biar autofilled di form
+  getJournalforCasesByID: async (req, res) => {
+    const { id } = req.params; //idjurnal
+    const data = await Journal.findOne({ _id: id });
+
+    if (data) {
+      res.status(200).json({
+        message: "data jurnal berhasil tampil",
+        data,
+      });
+    } else {
+      res.status(400).json({
+        message: "data jurnal tidak ditemukan",
+      });
+    }
+  },
+
+  //ini dari form pengajuan ketik pilih journal
+  getJournalById: async (req, res) => {
+    const Journal = req.query.JournalID || "";
+    const data = await Journal.find({ _id: Journal });
+    if (data) {
+      res.status(200).json({
+        message: "data jurnal berhasil ditampilkan",
+        data,
+      });
+    } else {
+      res.status(400).json({
+        message: "data jurnal tidak ditemukan",
+      });
+    }
   },
 
   getCasesById: async (req, res) => {
@@ -32,7 +72,7 @@ module.exports = {
 
   addCases: async (req, res) => {
     const { userId } = req.user;
-    const { title, description, category, message } = req.body;
+    const { title, description, category, message, journalID } = req.body;
 
     if (!title) {
       return res.json({
@@ -61,6 +101,57 @@ module.exports = {
         createdBy: userId,
         created: new Date(),
         isApproved: "Submitted",
+        journalID,
+      });
+      await newCases.save();
+      res.status(201).json({
+        message: "Berhasil Upload",
+        data: {
+          title,
+          description,
+          category,
+          message,
+        },
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: "Gagal upload Pengajuan Kasus",
+      });
+    }
+  },
+
+  addCasesDraft: async (req, res) => {
+    const { userId } = req.user;
+    const { title, description, category, message, journalID } = req.body;
+
+    if (!title) {
+      return res.json({
+        message: "Judul Kasus tidak boleh kosong",
+      });
+    }
+
+    if (!description) {
+      return res.json({
+        message: "Ringkasan Kasus tidak boleh kosong",
+      });
+    }
+
+    if (!category) {
+      return res.json({
+        message: "Kategori tidak boleh kosong",
+      });
+    }
+
+    try {
+      const newCases = new Cases({
+        title,
+        description,
+        category,
+        message,
+        createdBy: userId,
+        created: new Date(),
+        isApproved: "Draft",
+        journalID,
       });
       await newCases.save();
       res.status(201).json({
