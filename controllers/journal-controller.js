2f0c01d2f14ffc12c7f768a5d4cb60b26f49abb7
Rusydina Novitasari <rnvitas697@gmail.com>
2024-11-02 20:18:11 +0700

    Change user ID source from params to req.user payload; add category reference to journal schema

--- controllers/journal-controller.js
+++ controllers/journal-controller.js
@@ -1,97 +1,106 @@
-const Journal = require("../models/Journal")
+const Journal = require("../models/Journal");
 
 module.exports = {
-    getAllJournal: async (req, res) => {
-        const data = await Journal.find({}).populate("createdBy")
+  getAllJournal: async (req, res) => {
+    const data = await Journal.find({})
+      .populate("createdBy")
+      .sort({ created: "desc" });
 
+    res.json({
+      message: "Berhasil mendapatkan semua Journal",
+      data,
+    });
+  },
+  getDetailJournal: async (req, res) => {
+    const { id } = req.params;
+
+    const findJournal = await Journal.findById(id);
+
+    res.json({
+      message: "Berhasil mendapatkan detail Journal by id",
+      findJournal,
+    });
+  },
+  //vita: edit iduser diambil dari req.user, tambah sort created desc
+  getJournalByIdUser: (req, res) => {
+    const { userId } = req.user;
+
+    Journal.find({ createdBy: userId })
+      .populate("createdBy")
+      .sort({ created: "desc" })
+      .then((data) => {
         res.json({
-            message: "Berhasil mendapatkan semua Journal",
-            data
-        })
-    },
-    getDetailJournal: async (req, res) => {
-        const { id } = req.params
-
-        const findJournal = await Journal.findById(id)
-
+          message: "Berhasil mendapatkan Journal by id user",
+          data,
+        });
+      })
+      .catch((err) => {
         res.json({
-            message: "Berhasil mendapatkan detail Journal by id",
-            findJournal
-        })
-    },
-    getJournalByIdUser: (req, res) => {
-        const { id } = req.params
+          message: "Gagal mendapatkan Journal by id user",
+          err,
+        });
+      });
+  },
+  // Vita: tambah kondisi createdBy dari payload req.user
+  addJournal: (req, res) => {
+    const data = req.body;
+    const { userId } = req.userId;
 
-        Journal.find({ createdBy: id }).populate("createdBy")
-            .then(data => {
-                res.json({
-                    message: "Berhasil mendapatkan Journal by id user",
-                    data
-                })
-            })
-            .catch(err => {
-                res.json({
-                    message: "Gagal mendapatkan Journal by id user",
-                    err
-                })
-            })
-    },
-    addJournal: (req, res) => {
-        const data = req.body
+    //check if file uploaded
+    if (req.file) {
+      console.log("File uploaded: ", req.file);
+      data.file = `/journal-assets/${req.file.filename}`;
+    } else {
+      console.log("No file uploaded");
+    }
 
-        //check if file uploaded
-        if(req.file){
-            console.log("File uploaded: ", req.file)
-            data.file = `/journal-assets/${req.file.filename}`
-        }else{
-            console.log("No file uploaded")
-        }
-
-        const newJournal = new Journal(data)
-        newJournal.save()
-        .then((savedJournal) => {
-            res.json({
-            message: "Journal berhasil dibuat",
-            savedJournal
-            })
-        })
-        .catch((e) => {
-            console.log("Error saving journal", e)
-            res.json({
-                message: "Gagal buat Journal"
-            })
-        })
-        
-    },
-    editJournal: async (req, res) => {
-        const { id } = req.params
-
-        // const editDataJournal = await Journal.findByIdAndUpdate(id, req.body)
-        // editDataJournal.save()
-
-        const editData = {
-            ...req.body,
-            edited: new Date() //auto set edit date
-        }
-
-        if(req.file){
-            console.log("File uploaded: ", req.file)
-            editData.file = `/journal-assets/${req.file.filename}`
-        }
-
-        const updatedJournal = await Journal.findByIdAndUpdate(id, editData, { new: true })
+    const newJournal = new Journal({ ...data, createdBy: userId });
+    newJournal
+      .save()
+      .then((savedJournal) => {
         res.json({
-            message: "Berhasil update Journal",
-            updatedJournal
-        })
-    },
-    deleteJournal: async (req, res) => {
-        const { id } = req.params
-
-        await Journal.findByIdAndDelete(id)
-
+          message: "Journal berhasil dibuat",
+          savedJournal,
+        });
+      })
+      .catch((e) => {
+        console.log("Error saving journal", e);
         res.json({
-            message: "Berhasil hapus Journal"
-        })
-    },
-}
+          message: "Gagal buat Journal",
+        });
+      });
+  },
+  editJournal: async (req, res) => {
+    const { id } = req.params;
+
+    // const editDataJournal = await Journal.findByIdAndUpdate(id, req.body)
+    // editDataJournal.save()
+
+    const editData = {
+      ...req.body,
+      edited: new Date(), //auto set edit date
+    };
+
+    if (req.file) {
+      console.log("File uploaded: ", req.file);
+      editData.file = `/journal-assets/${req.file.filename}`;
+    }
+
+    const updatedJournal = await Journal.findByIdAndUpdate(id, editData, {
+      new: true,
+    });
+    res.json({
+      message: "Berhasil update Journal",
+      updatedJournal,
+    });
+  },
+  deleteJournal: async (req, res) => {
+    const { id } = req.params;
+
+    await Journal.findByIdAndDelete(id);
+
+    res.json({
+      message: "Berhasil hapus Journal",
+    });
+  },
+};
