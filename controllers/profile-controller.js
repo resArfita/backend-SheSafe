d348035604ada1313326da5894d71cbd155144b6
Rusydina Novitasari <rnvitas697@gmail.com>
2024-11-03 17:52:38 +0700

    Enhance getProfile and editProfile functions

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -2,23 +2,31 @@ const User = require("../models/User");
 
 module.exports = {
   getProfile: async (req, res) => {
-    const { userId } = req.user;
-    const data = await User.find({ _id: userId });
+    try {
+      const { userId } = req.user;
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "User tidak ditemukan" });
+      }
 
-    res.json({
-      message: "Berhasil mendapatkan semua User",
-      data: {
-        ...data,
-        fullName,
-        email,
-        gender,
-        birthDate,
-        avatar,
-      },
-    });
+      res.json({
+        message: "Berhasil mendapatkan data User",
+        data: {
+          fullName: user.fullName,
+          email: user.email,
+          gender: user.gender,
+          birthDate: user.birthDate,
+          avatar: user.avatar,
+        },
+      });
+    } catch (error) {
+      console.error(error); // Untuk debugging
+      res.status(500).json({ message: "Terjadi kesalahan pada server", error });
+    }
   },
 
   editProfile: async (req, res) => {
+    const { id } = req.params;
     const editData = {
       ...req.body,
       edited: new Date(), //auto set edit date
@@ -26,21 +34,20 @@ module.exports = {
 
     if (req.file) {
       console.log("File uploaded: ", req.file);
-      editData.file = `/userID-assets/${req.file.filename}`;
+      editData.avatar = `/userID-assets/${req.file.filename}`;
     }
 
-    const updatedUser = await User.findByIdAndUpdate(id, editData, {
+    const updatedUser = await User.findByIdAndUpdate({ _id: id }, editData, {
       new: true,
     });
     res.json({
       message: "Berhasil mengupdate user",
       data: {
-        ...updatedUser,
-        fullName,
-        email,
-        gender,
-        birthDate,
-        avatar,
+        fullName: updatedUser.fullName,
+        email: updatedUser.email,
+        gender: updatedUser.gender,
+        birthDate: updatedUser.birthDate,
+        avatar: updatedUser.avatar,
       },
     });
   },
