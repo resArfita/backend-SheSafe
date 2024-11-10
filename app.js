0199aa478c88fa38bdda98db2add1f218ec17e9e
Rusydina Novitasari <rnvitas697@gmail.com>
2024-11-11 00:47:59 +0700

    test

--- app.js
+++ app.js
@@ -12,17 +12,14 @@ app.use(
   cors({
     origin: function (origin, callback) {
       const allowedOrigins = process.env.ALLOWED_ORIGINS;
-      // const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
-
-      console.log("Incoming Origin:", origin);
-
+      console.log("Incoming Origin:", origin); // Debugging untuk memastikan origin diterima
       if (!origin) {
-        return callback(null, true); // Izinkan permintaan tanpa origin
+        return callback(null, true); // Izinkan permintaan tanpa origin (misalnya dari Postman)
       }
 
       if (allowedOrigins.indexOf(origin) === -1) {
         const msg = `This origin ${origin} is not allowed.`;
-        console.log(msg);
+        console.log(msg); // Jika origin tidak diizinkan
         return callback(new Error(msg), false);
       }
       return callback(null, true);
@@ -31,11 +28,10 @@ app.use(
     methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    credentials: true,
+    credentials: true, // Mengizinkan cookies dan kredensial lainnya
   })
 );
-
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json());
 db.then(() => {
